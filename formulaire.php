8b7b6508e12872da6492a02cdcf5598886e86daa
Davesimplon <david.scellier@gmail.com>
2019-02-11 11:46:03 +0400

    add php files

---
+++ formulaire.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>TP PHP</title>
+</head>
+<body>
+	
+	<form action="secret.php" method="POST">
+		<p> Enter the password
+			<input type="text" name="password" />
+			<input type="submit" name="valider" />
+		</p>
+	</form>
+
+	
+
+
+
+
+
+</body>
+</html>
